5a2a493e05b00e554d2811a0245ee30baa37a991
alessandro stoppato <alexrah@gmail.com>
2024-05-13 12:08:01 +0200

    src/index.ts: remove export headerFooterExist(), add export contentExists()

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export {default as cmpBypass} from "./cmpBypass";
-export { headerFooterExist,contentIsVisible,contentNotExist } from "./elementsExist";
+export { contentIsVisible,contentExists, contentNotExist } from "./elementsExist";
 export type {tCommonSelectors, tSelector, tTestURL } from "./playwrightTypes";
