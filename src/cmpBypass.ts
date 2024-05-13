ba2ae0ad3eb47bf09f99107484d43553575e892c
alessandro stoppato <alexrah@gmail.com>
2024-05-13 12:06:40 +0200

    src/cmpBypass.ts: move logger inside method

--- src/cmpBypass.ts
+++ src/cmpBypass.ts
@@ -2,12 +2,12 @@ import logger from "@alexrah/logger";
 import { type Page } from '@playwright/test';
 
 
-/**
- * edit .env file adding DEBUG=pw:tests to enable Logger
- * */
-const lg = new logger('CmpBypass');
-
 const cmpBypass = async (page:Page) => {
+
+    /**
+     * edit .env file adding DEBUG=pw:tests to enable Logger
+     * */
+    const lg = new logger('CmpBypass');
 
     lg.i('click on CMP');
     await page.locator('.qc-cmp2-summary-buttons button span',{hasText: 'ACCETTO'}).click();
