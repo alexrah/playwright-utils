857314abba86715b7fcfb8c10be9b9df13311ba0
alessandro stoppato <alexrah@gmail.com>
2024-07-22 20:24:41 +0200

    src/index.ts: export wpSessionLoginStorage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as cmpBypass} from "./cmpBypass";
 export { contentIsVisible,contentExists, contentNotExist } from "./elementsExist";
 export type {tCommonSelectors, tSelector, tTestURL } from "./playwrightTypes";
+export { wpSessionLoginStorage } from "./wpSessionLoginStorage"
